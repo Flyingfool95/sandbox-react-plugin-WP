6296cdfb67ba72e9a71449cb47e2b384b2185e8c
JohannesAtDexera <johannes@dexera.se>
2022-11-03 12:42:02 +0100

    responsive defaults

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '16db25f79cde80cb29c1');
+<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '811fd3f1268b869603c7');
